a2cd765793678ffc759a434cd67587280e072b80
IE3PMDP000046 <richard.neiel@gmail.com>
2016-07-01 19:09:04 +0600

    adding files

--- Water/SecondViewController.swift
+++ Water/SecondViewController.swift
@@ -1,25 +1,50 @@
 //
 //  SecondViewController.swift
-//  Water
+//  WaterManagement
 //
-//  Created by IE3PMDP000046 on 01/07/16.
+//  Created by IE3PMDP000046 on 27/06/16.
 //  Copyright © 2016 Honeywell. All rights reserved.
 //
 
 import UIKit
 
-class SecondViewController: UIViewController {
 
+class SecondViewController: UIViewController {
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        var Locations = ["Gachibowli",
+                         "Nankram Guda",
+                         "Golconda fort",
+                         "Ghansi Bazaar",
+                         "Mehdipatnam",
+                         "kachiguda",
+                         "Medowli",
+                         "Miyapur",
+                         "Nizampet",
+                         "Manikonda",
+                         "Madhapur",
+                         "HMT Colony",
+                         "Secunderabad",
+                         "Kothapet Village",
+                         "Nadergul",
+                         "ECIL",
+                         "Shamshabad",
+                         "Saidabad",
+                         "Jubilee hills",
+                         "Gandhi nagar"]
+        
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+   
 
 }
 
